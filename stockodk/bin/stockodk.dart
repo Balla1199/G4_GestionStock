deeb05d04efec86d1e7d75b32db131681558b7e5
balla1199 <ballacoulibaly187@gmail.com>
2024-05-10 11:07:04 +0100

    Première push

---
+++ stockodk/bin/stockodk.dart
@@ -0,0 +1,5 @@
+import 'package:stockodk/stockodk.dart' as stockodk;
+
+void main(List<String> arguments) {
+  print('Hello world: ${stockodk.calculate()}!');
+}
---
+++ stockodk/test/stockodk_test.dart
@@ -0,0 +1,8 @@
+import 'package:stockodk/stockodk.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
