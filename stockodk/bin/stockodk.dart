e643cbc18e48e3428a63433e278bf62eaba8aaf5
Mcoulby91 <manacoulibaly44@gmail.com>
2024-05-14 17:18:46 +0000

    correction

--- stockodk/bin/stockodk.dart
+++ stockodk/bin/stockodk.dart
@@ -1,4 +1,12 @@
 import 'package:stockodk/stockodk.dart' as stockodk;
+import 'package:stockodk/models/bon_entree.dart';
 void main(List<String> arguments) {
-  print('Hello world: ${stockodk.calculate()}!');
+
+  // Ajout des bons d'entrée à la liste
+  BonEntree.ajouterBonEntree(1, "2023-05-14", "En cours", 123, 456);
+  BonEntree.modifierUnBonEntee(1, dateDeCommande: "2023-78-00", );
+  // Affichage des bons d'entrée
+  BonEntree.afficherBonsEntree();
+
+  // BonEntree.rechercherBonEntreeParId(2);
 }
--- stockodk/lib/models/bon_entree.dart
+++ stockodk/lib/models/bon_entree.dart
@@ -6,48 +6,60 @@ class BonEntree
   int idUtilisateur;
   int idFournisseur;
 
-  // Liste pour stocker les bons d'entrée
-    static List<BonEntree> bonsEntree = [];
-
   // Constructeur de la classe
     BonEntree(this.bonEntreeId, this.dateDeCommande, this.statut, this.idUtilisateur, this.idFournisseur);
+    static List<BonEntree> bon_entree = [];
 
   // Méthode pour ajouter un bon d'entrée à la liste
-    void ajouterBonEntree() 
+    static void ajouterBonEntree(bonEntreeId, dateDeCommande, statut, idUtilisateur, idFournisseur) 
     {
-      bonsEntree.add(this);
+      bon_entree.add(BonEntree(bonEntreeId, dateDeCommande, statut, idUtilisateur, idFournisseur));
+      print("ajout effectuer avec succes");
     }
 
   // Méthode pour afficher tous les bons d'entrée
     static void afficherBonsEntree()
     {
-      for (var bon in bonsEntree)
+      for (var bon in bon_entree)
       {
-        print("Bon d'entrée ID : ${bon.bonEntreeId}");
-        print("Date de commande : ${bon.dateDeCommande}");
-        print("Statut : ${bon.statut}");
-        print("ID utilisateur : ${bon.idUtilisateur}");
-        print("ID fournisseur : ${bon.idFournisseur}");
-        print('---');
+        print("Bon d'entrée ID : ${bon.bonEntreeId}, Date de commande : ${bon.dateDeCommande}, Statut : ${bon.statut}, ID utilisateur : ${bon.idUtilisateur},ID fournisseur : ${bon.idFournisseur}");
       }
     }
+
+  // Méthode pour modifier un bon d'éntrée
+   static modifierUnBonEntee (int? bonEntreeId,{
+  String? dateDeCommande,
+  String? statut,
+  int? idUtilisateur,
+  int? idFournisseur})
+  {
+        for (var bon in bon_entree)
+        {
+            if (bon.bonEntreeId == bonEntreeId)
+            {
+              if (dateDeCommande != null) bon.dateDeCommande = dateDeCommande;
+              if (statut != null) bon.statut = statut;
+              if (idFournisseur != null) bon.idFournisseur = idFournisseur;
+              if (idUtilisateur != null) bon.idUtilisateur = idUtilisateur;
+              print("Le bon ${bon.bonEntreeId} a été modofier avec succè");
+            } else{
+              print("Entrer un ID valide !");
+              }
+        }
+  }
   // Méthode de recherche de bon d'entrée par son Id
-   static void rechercherBonEntreeParId(int id) 
+   static rechercherBonEntreeParId(int id) 
     {
-      for (var bon in bonsEntree) 
+      for (var bon in bon_entree) 
       {
         if (bon.bonEntreeId == id)
         {
           print("Bon d'entrée trouvé :");
-          print("Bon d'entrée ID : ${bon.bonEntreeId}");
-          print("Date de commande : ${bon.dateDeCommande}");
-          print("Statut : ${bon.statut}");
-          print("ID utilisateur : ${bon.idUtilisateur}");
-          print("ID fournisseur : ${bon.idFournisseur}");
-        }
+          print("Bon d'entrée ID : ${bon.bonEntreeId}, Date de commande : ${bon.dateDeCommande}, Statut : ${bon.statut}, ID utilisateur : ${bon.idUtilisateur},ID fournisseur : ${bon.idFournisseur}");
+
+        } else print("Ce bon d'entré n'existe pas dans la liste, entrer un ID valide ");
       }
     
     }
  
 }
-
