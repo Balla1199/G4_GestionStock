d1693af1e4bb46d5562170e9837956ec61c882eb
reyptz <reypotozy@gmail.com>
2024-05-16 16:36:51 +0000

    pUSH by Moi en faveur de Balla !

--- stockodk/lib/models/details_entree.dart
+++ stockodk/lib/models/details_entree.dart
@@ -1,33 +1,98 @@
+import 'dart:io';
+
 class DetailsEntree {
-  // Attributs
   int id;
   int quant;
   double prixtotal;
   int idprod;
   int idbonentree;
-  // constructeur
-  DetailsEntree( this.id, this.quant, this.prixtotal, this.idprod, this.idbonentree);
-  //Liste en memoire pour stocker les details sorties
+
+  DetailsEntree(this.id, this.quant, this.prixtotal, this.idprod, this.idbonentree);
+
   static List<DetailsEntree> _detailsEntrees = [];
-  // Fonctionnalité de creation d'un détails entrée
-    void creerDetailsEntree(DetailsEntree detailsEntree){
-      _detailsEntrees.add(detailsEntree);
+
+  void creerDetailsEntree() {
+    print("Entrez l'ID :");
+    id = int.parse(stdin.readLineSync()!);
+    print("Entrez la quantité :");
+    quant = int.parse(stdin.readLineSync()!);
+    print("Entrez le prix total :");
+    prixtotal = double.parse(stdin.readLineSync()!);
+    print("Entrez l'ID du produit :");
+    idprod = int.parse(stdin.readLineSync()!);
+    print("Entrez l'ID du bon d'entrée :");
+    idbonentree = int.parse(stdin.readLineSync()!);
+
+    _detailsEntrees.add(this);
+    print("Détail d'entrée créé avec succès.");
+  }
+
+  void modifierDetailsEntree() {
+    print("Entrez le nouvel ID :");
+    id = int.parse(stdin.readLineSync()!);
+    print("Entrez la nouvelle quantité :");
+    quant = int.parse(stdin.readLineSync()!);
+    print("Entrez le nouveau prix total :");
+    prixtotal = double.parse(stdin.readLineSync()!);
+    print("Entrez le nouvel ID du produit :");
+    idprod = int.parse(stdin.readLineSync()!);
+    print("Entrez le nouvel ID du bon d'entrée :");
+    idbonentree = int.parse(stdin.readLineSync()!);
+
+    print("Détail d'entrée modifié avec succès.");
+  }
+
+  void supprimerDetailsEntree() {
+    print("Entrez l'ID du détail à supprimer :");
+    var idToDelete = int.parse(stdin.readLineSync()!);
+    _detailsEntrees.removeWhere((detailsEntree) => detailsEntree.id == idToDelete);
+    print("Détail d'entrée supprimé avec succès.");
+  }
+
+  void rechercherDetailsEntreeParID() {
+    print("Entrez l'ID du détail d'entrée à rechercher :");
+    var idRecherche = int.parse(stdin.readLineSync()!);
+
+    var detailTrouve = _detailsEntrees.firstWhere((detailsEntree) => detailsEntree.id == idRecherche);
+
+    if (detailTrouve != null) {
+      print("Détail d'entrée trouvé :");
+      print("ID: ${detailTrouve.id}, Quantité: ${detailTrouve.quant}, Prix Total: ${detailTrouve.prixtotal}, ID du Produit: ${detailTrouve.idprod}, ID du Bon d'Entrée: ${detailTrouve.idbonentree}");
+    } else {
+      print("Aucun détail d'entrée trouvé pour l'ID spécifié.");
     }
-  // Fonctionnalité de modification d'un détail d'entrée
-  void modifierDetailsEntree(DetailsEntree detailsEntreemodifie){
-     int index = _detailsEntrees.indexWhere((detailsEntree) => detailsEntree.id == detailsEntreemodifie.id);
-     if (index!= -1) {
-_detailsEntrees [index]= detailsEntreemodifie;
-     } else{
-      print("Detail entree non trouvé");
-     }
   }
-  // Fonctionnalité de suppression d'un détail d'entrée
-  void supprimerDetailsEntree(int id){
-     _detailsEntrees.remove((detailsEntree)=> detailsEntree.id == id);
-  }
-  
+}
 
-  }
+// void main() {
+  // var detailsEntree = DetailsEntree(0, 0, 0, 0, 0);
+  // print("Bienvenue ! Que souhaitez-vous faire ?");
+  // print("----------------------------------")
+  // print("|1. Créer un détail d'entrée     |");
+  // print("----------------------------------")
+  // print("|2. Modifier un détail d'entrée  |");
+  // print("----------------------------------")
+  // print("|3. Supprimer un détail d'entrée |");
+  // print("----------------------------------")
+  // print("|4. Rechercher un détail d'entrée|");
+  // print("----------------------------------")
+  // var choix = int.parse(stdin.readLineSync()!);
 
-    
+  // switch (choix) {
+    // case 1:
+      // detailsEntree.creerDetailsEntree();
+      //break;
+    // case 2:
+      // detailsEntree.modifierDetailsEntree();
+      //break;
+    // case 3:
+      // detailsEntree.supprimerDetailsEntree();
+      // break;
+   // case 4:
+      // detailsEntree.rechercherDetailsEntreeParID();
+      // break;
+    // default:
+      // print("Choix invalide.");
+      // break;
+  // }
+// }
